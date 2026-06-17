54e2d65fba76f08e4e1b462d56837c384a7d9c40
Ivikah <209543083+Ivikah@users.noreply.github.com>
2026-06-17 23:22:28 +0200

    fix

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
+local Players = game:GetService("Players")
+local enemiesFolder = workspace.Enemies
 local hrp = Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-local enemies = workspace.Enemies:GetChildren()
-for i, enemy in ipairs(enemies) do
-    local enemyhrp = enemy:FindFirstChild("HumanoidRootPart")
-    if enemyhrp then
-        hrp.Cframe = enemyhrp.Cframe.Position + Vector3.new(0, 30, 0)
-    end
+local x = true
+while x == true do
+    local enemyList = enemiesFolder:GetChildren()
+    task.wait(0.1)
+    for i, enemy in ipairs(enemyList) do
+          local enemyhrp = enemy:FindFirstChild("HumanoidRootPart")
+     if enemyhrp then
+          enemyhrp.CFrame = hrp.CFrame + Vector3.new(0, 0, 3)
+        end
+end
 end
